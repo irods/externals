bb3bf00254f6b196e84f6d5b7f4bd66814e183b6
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2025-05-20 15:48:18 -0500

    [irods/irods_7726] Remove nlohmann-json

--- build.py
+++ build.py
@@ -214,8 +214,6 @@ def build_package(target, build_native_package):
     log.debug('boost_root: [{0}]'.format(boost_root))
     fmt_root = get_local_path('fmt',[])
     log.debug('fmt_root: [{0}]'.format(fmt_root))
-    json_root = get_local_path('json',[])
-    log.debug('json_root: [{0}]'.format(json_root))
 
     # build boost install path
     boost_info = get_versions()['boost']
@@ -333,7 +331,6 @@ def build_package(target, build_native_package):
         i = re.sub("TEMPLATE_CPPZMQ_PATH", cppzmq_root, i)
         i = re.sub("TEMPLATE_FMT_PATH", fmt_root, i)
         i = re.sub("TEMPLATE_FMT_RPATH", fmt_rpath, i)
-        i = re.sub("TEMPLATE_JSON_PATH", json_root, i)
         run_cmd(i, run_env=myenv, unsafe_shell=True, check_rc='build failed')
 
     # package
